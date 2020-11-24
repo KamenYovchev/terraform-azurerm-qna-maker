874eb8d79406ce0e323b1741f4164f1a989e5264
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2020-11-24 18:19:12 +0200

    add resources to repo

--- variables.tf
+++ variables.tf
@@ -38,8 +38,3 @@ variable "plan_id" {
   default = ""
   description = "The app service plan to use.  If none is passed it will create one"
 }
-
-
-variable "plan_kind" {
-  default = "Windows"
-  description = "The kind of the App Service Plan to create. Possible values are Windows (also available as App), Linux, elastic (for Premium Consumption) and FunctionApp (for a Consumption Plan). Defaults to Windows. Changing this forces a new resource to be created."
